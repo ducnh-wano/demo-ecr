e481005e0994661c2d24afb92281d9d47783acde
ducnh-wano <nguyentuan@team-lab.com>
2022-10-07 02:21:23 -0700

    test

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const HOST = '0.0.0.0';
 const app = express();
 app.get('/', (req, res) => {
   res.send(
-    `<body style='background-color:#283E5B'><h1 style='color: orange;text-align:center'>Hello Nguyen Hoai Duc ${os.hostname()}</h1></body>`
+    `<body style='background-color:#283E5B'><h1 style='color: orange;text-align:center'>Hello Tu Ngu ${os.hostname()}</h1></body>`
   );
 });
 
